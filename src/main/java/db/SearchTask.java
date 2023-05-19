d6af8045bc6c84fcf09b22933cc9fb47cc91c5df
cyberknugen <anna.hakansson@pm.me>
2023-05-19 13:37:18 +0200

    searchbuilder flyttat till inre klass i searchtask

--- src/main/java/db/SearchTask.java
+++ src/main/java/db/SearchTask.java
@@ -9,7 +9,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import marshall.UnmarshallHandler;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class SearchTask extends DBtask {
 
@@ -96,7 +98,56 @@ public class SearchTask extends DBtask {
         return query;
     }
 
+    private class SearchBuilder {
+        private Connection connection;
+        private List<Object> parameters = new ArrayList<>();
+        private StringBuilder query = new StringBuilder();
 
+        public SearchBuilder(Connection connection) {
+            this.connection = connection;
+            query.append("SELECT * FROM products WHERE status = ?");
+            parameters.add("available");
+        }
 
+        public SearchBuilder productType(String productType) {
+            if (productType != null && !productType.isEmpty()) {
+                query.append(" AND producttype LIKE ?");
+                parameters.add("%" + productType + "%");
+            }
+            return this;
+        }
+
+        public SearchBuilder minPrice(float minPrice) {
+            if (minPrice >= 0) {
+                query.append(" AND price >= ?");
+                parameters.add(minPrice);
+            }
+            return this;
+        }
+
+        public SearchBuilder maxPrice(float maxPrice) {
+            if (maxPrice >= 0) {
+                query.append(" AND price <= ?");
+                parameters.add(maxPrice);
+            }
+            return this;
+        }
+
+        public SearchBuilder condition(String condition) {
+            if (condition != null && !condition.isEmpty()) {
+                query.append(" AND conditions = ?");
+                parameters.add(condition);
+            }
+            return this;
+        }
+
+        public PreparedStatement build() throws SQLException {
+            PreparedStatement statement = connection.prepareStatement(query.toString());
+            for (int i = 0; i < parameters.size(); i++) {
+                statement.setObject(i + 1, parameters.get(i));
+            }
+            return statement;
+        }
+    }
 }
 
